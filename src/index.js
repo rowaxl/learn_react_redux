3a00c0fd179f5379077e9674d90d3834e32f2188
rowaxl <rowaxl0@gmail.com>
2018-12-10 23:13:00 +0900

    learn basic of jsx start learning react props replace to class pattern implement sementic-ui Use JSON to load comment list update webpack-dev-server@ add message component add state practice delete useless sample code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
+// Import the React and ReactDoM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import Faker from 'faker';
+import comments from './commentList.json';
+import Message from './Message';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-css/semantic.min.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create a react component
+class App extends React.Component {
+    render() {
+        // mapping pattern
+        const CommentDetailList = comments.list.map((prop, i) => {
+            return (
+                <ApprovalCard key={i * 65535}>
+                    <CommentDetail
+                        key={i}
+                        avatar={Faker.image.avatar()}
+                        author={prop.author}
+                        date={prop.date}
+                        content={prop.content} />
+                </ApprovalCard>
+            );
+        });
+
+        return (
+            <div className="ui container comments" style={{ marginTop: "10px" }}>
+                <Message
+                    header="Change in Services"
+                    message="We just updated privacy policy." />
+                {CommentDetailList}
+            </div>
+        );
+    }
+    
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
