9b91d52194715f773ff23caa0fcfa0817fdb60f9
= <rowaxl0@mgail.com>
2018-12-16 21:16:06 +0900

    start learning react props

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
 // Import the React and ReactDoM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import Faker from 'faker';
+
+const commentList = [
+    {
+        author: "Amita",
+        date: "Today, 06:00 PM",
+        content: "Nice blog post!",
+        avatar: Faker.image.avatar(),
+    },
+    {
+        author: "Aimin",
+        date: "Today, 04:00 PM",
+        content: "Nice start!",
+        avatar: Faker.image.avatar(),
+    },
+    {
+        author: "Kudo-san",
+        date: "Yesterday, 11:00 AM",
+        content: "That awesome! I think you did very good.",
+        avatar: Faker.image.avatar(),
+    },
+    {
+        author: "OzaPure",
+        date: "12/16, 10:00 AM",
+        content: "Another React noob.",
+        avatar: Faker.image.avatar(),
+    },
+    {
+        author: "Pressure",
+        date: "12/10, 09:00 PM",
+        content: "Welcome",
+        avatar: Faker.image.avatar(),
+    },
+];
 
 // Create a react component
 const App = () => {
-    const submitButtonText = {message:'Submit'};
-    const style = { backgroundColor: 'blue', color: 'white', border: '1px solid red', };
+    // コンポネントループパターン
+    const CommentDetailList = [];
+    commentList.forEach(prop => {
+        CommentDetailList.push(
+            <CommentDetail
+                avatar={prop.avatar}
+                author={prop.author}
+                date={prop.date}
+                content={prop.content} />
+        );
+    });
+
+    // render
     return (
-        <div>
-            <label className="label" for="name">Name:</label>
-            <input id="name" type="text" />
-            <button style={style}>
-                {submitButtonText.message}
-            </button>
-        </div>);
+        <div className="ui container comments">
+            {CommentDetailList}
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
