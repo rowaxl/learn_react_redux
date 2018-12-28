cd1545f40fdefa18f180a8c614d80af78db1e87a
= <rowaxl0@mgail.com>
2018-12-28 21:31:45 +0900

    Use JSON to load comment list

--- src/index.js
+++ src/index.js
@@ -4,53 +4,21 @@ import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 import Faker from 'faker';
+import comments from './commentList.json';
 
 import 'semantic-ui-css/semantic.min.css';
 
 // Create a react component
 class App extends React.Component {
     render() {
-        const commentList = [
-            {
-                author: "Amita",
-                date: "Today, 06:00 PM",
-                content: "Nice blog post!",
-                avatar: Faker.image.avatar(),
-            },
-            {
-                author: "Aimin",
-                date: "Today, 04:00 PM",
-                content: "Nice start!",
-                avatar: Faker.image.avatar(),
-            },
-            {
-                author: "Kudo-san",
-                date: "Yesterday, 11:00 AM",
-                content: "That awesome! I think you did very good.",
-                avatar: Faker.image.avatar(),
-            },
-            {
-                author: "OzaPure",
-                date: "12/16, 10:00 AM",
-                content: "Another React noob.",
-                avatar: Faker.image.avatar(),
-            },
-            {
-                author: "Pressure",
-                date: "12/10, 09:00 PM",
-                content: "Welcome",
-                avatar: Faker.image.avatar(),
-            },
-        ];
-
         // ループパターン
-        const CommentDetailList = commentList.map((prop, i) => {
+        const CommentDetailList = comments.list.map((prop, i) => {
             console.log("key", i);
             return (
                 <ApprovalCard key={i * 65535}>
                     <CommentDetail
                         key={i}
-                        avatar={prop.avatar}
+                        avatar={Faker.image.avatar()}
                         author={prop.author}
                         date={prop.date}
                         content={prop.content} />
@@ -59,7 +27,7 @@ class App extends React.Component {
         });
 
         return (
-            <div className="ui container comments">
+            <div className="ui container comments" style={{marginTop: "10px"}}>
                 {CommentDetailList}
             </div>
         );
